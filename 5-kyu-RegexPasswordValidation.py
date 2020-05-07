abf10473effb6402d38f7cf0ad85349a85b04e2f
Kacper Cieślak <62593590+Kacper-Cieslak@users.noreply.github.com>
2020-05-07 17:31:22 +0200

    Create 5-kyu-RegexPasswordValidation.py

---
+++ 5-kyu-RegexPasswordValidation.py
@@ -0,0 +1,19 @@
+'''
+You need to write regex that will validate a password to make sure it meets the following criteria:
+
+At least six characters long
+contains a lowercase letter
+contains an uppercase letter
+contains a number
+Valid passwords will only be alphanumeric characters.
+'''
+
+#Solution:
+
+import re
+def validation(password):
+
+    if re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$", password):
+        return True
+    else:
+        return False
